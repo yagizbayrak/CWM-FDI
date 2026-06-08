1eab64578e32145b72bbd783cea1edc1e09a0567
Live session user <yagizbayrak01@gmail.com>
2026-06-08 15:24:15 +0100

    Plots

---
+++ assignment2/plot.py
@@ -0,0 +1,38 @@
+# !/usr/bin/python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# parameters to modify 
+filename="interval_00001.data"
+label='c'
+xlabel = 'runs'
+ylabel = 'time'
+title='Running time across runs'
+fig_name='assignment2_plot_4.png'
+bins=100 #adjust the number of bins to your plot
+
+## load data from input file
+t = np.loadtxt(filename, delimiter=" ", dtype="float")
+
+## if your data is "X Y" (2 cols), use the following line
+#plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
+
+## if your data is "X" (1 col), use the following line
+#plt.plot(t, label=label)  # Plot some data on the (implicit) axes.
+
+## comment the lines above and uncomment the line below to plot a simple CDF
+plt.hist(t[:], bins, density=True, histtype='step', cumulative=True, label=label)
+
+## comment the lines above and uncomment the 4 lines below for a nicer CDF
+#n = np.arange(1,len(t)+1) / float(len(t))
+#ts = np.sort(t)
+#fig, ax = plt.subplots()
+#ax.step(ts,n)
+
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.title(title)
+plt.legend()
+plt.savefig(fig_name)
+plt.show()
